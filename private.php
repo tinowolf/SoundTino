80c7300349355457cf2b2089ddd54fcf486eb3bc
tinowolf <32926994+tinowolf@users.noreply.github.com>
2018-06-11 12:45:17 +0200

    qwertyu

--- private.php
+++ private.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Private 3</title>
+		<title>Private</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
